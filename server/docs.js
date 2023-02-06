48171efbaf425a8ab1c58b87919ab992b8b9b42a
Christopher Lin <powertothepenguins@gmail.com>
2023-02-06 12:19:09 -0800

    Ignore prefix numbers in titles only if followed by delimiter
    Meaningful numbers were being cut out, e.g. "447" in "447 Minna" and
    "1997:" in Andie's 1997 play from "30 Plays for 30 Years."

--- server/docs.js
+++ server/docs.js
@@ -20,7 +20,7 @@ exports.cleanName = (name = '') => {
   return name
     .trim()
     // eslint-disable-next-line no-useless-escape
-    .replace(/^(\d+[-–—_\s]*)([^\d\/\-^\s]+)/, '$2') // remove leading numbers and delimiters
+    .replace(/^(\d+\s+[-–—_]+\s+)([^\d\/\-^\s]+)/, '$2') // remove leading numbers and delimiters
     .replace(/\s*\|\s*([^|]+)$/i, '') // remove trailing pipe and tags
 
     // commented out for SF Neo-Futurists b/c some play titles use periods in
